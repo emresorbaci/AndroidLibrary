18c915ee381be8d3e4961f8d8c6990fe3199bc67
EMRE SORBACI <emresorbaci00@gmail.com>
2017-04-21 01:38:45 +0300

    MyTextView font deleted.

--- logutillib/src/main/java/com/mylogutil/emre/logutillib/MyTextView.java
+++ logutillib/src/main/java/com/mylogutil/emre/logutillib/MyTextView.java
@@ -2,7 +2,6 @@ package com.mylogutil.emre.logutillib;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
@@ -14,22 +13,16 @@ public class MyTextView extends TextView {
 
     public MyTextView(Context context) {
         super(context);
-        Typeface face = Typeface.createFromAsset(context.getAssets(), "murray.ttf");
-        this.setTypeface(face);
         this.setTextColor(10841354);
     }
 
     public MyTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        Typeface face = Typeface.createFromAsset(context.getAssets(), "murray.ttf");
-        this.setTypeface(face);
         this.setTextColor(10841354);
     }
 
     public MyTextView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        Typeface face = Typeface.createFromAsset(context.getAssets(), "murray.ttf");
-        this.setTypeface(face);
         this.setTextColor(10841354);
     }
 
